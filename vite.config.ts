13517748144aea5905bf497ae1d34099abecee75
United-Foxes <lucho9007@gmail.com>
2026-08-17 09:15:34 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Dao-Chang/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
